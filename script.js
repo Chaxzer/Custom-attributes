e62ef2f603ee6dd60a07f91a6db13e0b7efa09eb
songokudemonkil <22909087-songokudemonkil@users.noreply.replit.com>
2024-01-16 01:54:36 +0000

    use a custom attribute in a div or any other element using data-**="XXX"  and call it using datasetproperty   eg. idname.dataset.customsattribute

---
+++ script.js
@@ -0,0 +1,4 @@
+// document.write(custAtt.dataset.attId);
+ // document.getElementById("div1").innerHTML= document.getElementById("custAtt").dataset.myatt;
+  document.getElementById("div2").innerHTML= document.getElementById("custAtt").dataset.attid;
+console.log(custAtt.dataset.myatt)
